dde2bf0a90d72166640ca1723dedac3b45a40cc0
Akshat Tiwari <akshatt794@gmail.com>
2025-07-19 18:17:53 +0530

    verngrnsur

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 const express = require('express');
 const router = express.Router();
+const serviceAccount = require("../firebaseServiceKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
