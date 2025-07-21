19d1bb13b5b6762a836fb024f8123f1c418673f1
Akshat Tiwari <akshatt794@gmail.com>
2025-07-21 17:51:54 +0530

    nbwhbwubuwb

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,5 @@
+// /utils/cloudinary.js
 const cloudinary = require('cloudinary').v2;
-const { Readable } = require('stream');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,17 +7,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadFromBuffer = (buffer, folder) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      { resource_type: 'raw', folder },
-      (error, result) => {
-        if (result) resolve(result);
-        else reject(error);
-      }
-    );
-    Readable.from(buffer).pipe(stream);
-  });
-};
-
-module.exports = { cloudinary, uploadFromBuffer };
+module.exports = cloudinary;
