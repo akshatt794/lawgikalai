56ba453155e042ba93b221c8e8a637dd9ae192dc
Akshat Tiwari <akshatt794@gmail.com>
2025-07-29 16:02:10 +0530

    amiananwanaw

--- app.js
+++ app.js
@@ -83,8 +83,9 @@ app.get('/', (req, res) => {
 
 // 6. MongoDB Connect
 mongoose.connect(process.env.MONGODB_URI, {
-  useUnifiedTopology: true,
-})
+  useNewUrlParser: true
+});
+
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
